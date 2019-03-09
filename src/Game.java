fe22e113a06629e59a4de4a73dc0bc04f3924561
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-03-09 17:58:49 +0100

    Check if robot is on move

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,16 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 
 public class Game {
 
-	private static final String IP = "127.0.0.1";//"192.168.0.11";
+	private static final String IP = "192.168.0.11";
 	private static final int PORT = 59002;
 
 	private static final int MAX_PLY = 4;
+	private static final int GAME_STATE_ROUNDS = 5;
 
 	private SocketManager socketManager;
 	private Scanner scanner;
@@ -32,7 +34,7 @@ public class Game {
 
 		try {
 			while (true) {
-				int[][] gameState = readInputData();
+				int[][] gameState = getGameStateToPlay();
 				Board board = new Board(gameState);
 
 				MinMax.run(Board.State.O, board, MAX_PLY);
@@ -47,6 +49,30 @@ public class Game {
 		scanner.close();
 	}
 
+	private int[][] getGameStateToPlay(){
+
+		int sameRounds = 1;
+
+		int [][] gameState = readInputData();
+
+		while (GAME_STATE_ROUNDS > sameRounds) {
+			int [][] tmpGameState = readInputData();
+
+			if (isEmpty(tmpGameState)) continue; // First player must be human
+			if (!isOnMove(tmpGameState)) continue;
+
+			if (areEquals(tmpGameState, gameState)) {
+				sameRounds++;
+				System.out.println("Games are equals");
+			} else {
+				sameRounds = 1;
+				gameState = tmpGameState;
+				System.out.println("Games are not equals. Reset counter.");
+			}
+		}
+		return gameState;
+	}
+
 	private int[][] readInputData(){
 		String[] input = scanner.next().split(",");
 
@@ -56,5 +82,44 @@ public class Game {
 		}
 
 		return gameState;
+	}
+
+	private boolean isOnMove(int [][] gameState){
+		int player1 = 0;
+		int player2 = 0;
+		for (int[] ints : gameState) {
+			for (int anInt : ints) {
+				if (anInt == 1) player1++;
+				else if (anInt == 2) player2++;
+			}
+		}
+		boolean result = player1 > player2;
+
+		if(result) System.out.println("I am on move");
+		else System.out.println("I am not on move");
+
+		return result;
+	}
+
+	private boolean isEmpty(int[][] gameState){
+		for (int[] ints : gameState) {
+			for (int anInt : ints) {
+				if (anInt != 0) {
+					System.out.println("Input array is empty");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean areEquals(int[][] first, int[][] second){
+		if (first.length != second.length) return false;
+
+		for (int i = 0; i < first.length; i++) {
+			if(!Arrays.equals(first[i], second[i])) return false;
+		}
+
+		return true;
 	}
 }
